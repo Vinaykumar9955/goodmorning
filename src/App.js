ef7f002b06218bfa818532d186ae776bb94d3c8d
Vinay Kumar <kvina7288@gmail.com>
2024-05-29 21:19:04 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from "react";
+
+function App () {
+    
+let curDate = new Date(2030,3,4,10);
+curDate = curDate.getHours();
+let greeting = "";
+const cssStyle = {};
+
+if(curDate >= 1 && curDate <12) {
+    greeting = "Good Morning";
+    cssStyle.color = "Green";
+}
+else if (curDate>=12 && curDate <19){
+    greeting = "Good Afternoon";
+    cssStyle.color = "Orange";
+}else {
+    greeting = "Good Night";
+    cssStyle.color = "Black";
+}
+
+return (
+
+<>
+    <div>
+        <h1>
+            Hello Sir,<span style = {cssStyle}>{greeting}</span>
+        </h1>
     </div>
-  );
+</>
+
+
+);
+
+
 }
 
 export default App;
